6b90f811022c655b0946219552b7055992f6b38b
= <parthivhirani2903@gmail.com>
2023-08-07 19:25:31 +0530

    Commit for Repository Pattern

---
+++ RepositoryPattern/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using RepositoryPattern.Models;
+
+namespace RepositoryPattern.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        List<EmployeeR> GetAllEmployees(int? id);
+
+        bool CreateEmployee(EmployeeR employee);
+
+        bool EditEmployee(int id, EmployeeR employee);
+
+        bool DeleteEmployee(int id);
+    }
+}
